64dcdc7c9b3c325eb7803645650c5f59aa396047
azureberry <azureberry@eclipse>
2013-06-29 02:42:50 +0900

    variable

--- testRubyApp1/main.rb
+++ testRubyApp1/main.rb
@@ -1,4 +1,13 @@
-#! ruby -Ku
-# -*- coding: utf-8 -*-
+#! ruby -Ks
+# -*- coding: shift_jis -*-
+
 
 puts "Hello World"
+puts "ほげほげ"
+
+
+Var="定数"           #先頭を大文字
+var="ローカル変数"
+$var="グローバル変数"   #先頭を $
+@var="インスタンス変数"  #先頭を @
+@@var="クラス変数"     #先頭を @@
