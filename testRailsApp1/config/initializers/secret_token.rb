07b43b9275d99c34fdfe1bc971c147958f95cb65
azureberry <azureberry@>
2013-07-02 23:30:13 +0900

    rails test

---
+++ testRailsApp1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestRailsApp1::Application.config.secret_key_base = '3b7eb7962722b8753fef221638107dd558cb8c1448417ad94a4eea174eb07ee7ce6c4fc7bf9d40dea776b4079151e1a4ca14dea926a3a23cee0a992fd786aca2'
---
+++ testRailsApp1/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestRailsApp1::Application.config.session_store :cookie_store, key: '_testRailsApp1_session'
